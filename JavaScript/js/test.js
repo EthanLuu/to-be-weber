34eb94ffdff6e025e9343baaa5c1a2ee93d4d1a7
ethanol <952792901@qq.com>
2021-05-30 15:58:59 +0800

    chore: keep learning

--- JavaScript/js/test.js
+++ JavaScript/js/test.js
@@ -1,16 +1,24 @@
-let observer = new MutationObserver((mutationRecords) => {
-  console.log(mutationRecords)
-})
+async function getUsers(names) {
+  let jobs = []
+  for (let name of names) {
+    let response = fetch(`https://api.github.com/users/${name}`).then(
+      async (response) => {
+        const data = await response.json()
+        if (response.ok) {
+          return data
+        } else {
+          return null
+        }
+      }
+    )
+    jobs.push(response)
+  }
+  let res = await Promise.all(jobs)
+  return res
+}
 
-const elem = document.querySelector('#elem')
-
-observer.observe(elem, {
-  childList: true,
-  subtree: true,
-  characterDataOldValue: true,
-})
-
-const btn = document.querySelector('#btn')
-btn.addEventListener('click', () => {
-  elem.textContent = 'wow'
-})
+async function test() {
+  let users = await getUsers(['iliakan', 'remy', 'no.such.users'])
+  console.log(users)
+}
+test()
